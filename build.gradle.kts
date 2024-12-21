1d3c30e2a1ea1ac1af336808d9802fd686ee7ce4
hyun <shun9067@gmail.com>
2024-12-22 01:18:45 +0900

    설정 추가

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,8 @@
 plugins {
+    id("org.springframework.boot") version "2.2.0.RELEASE"
+    id("io.spring.dependency-management") version "1.0.8.RELEASE"
     kotlin("jvm") version "1.9.23"
+    kotlin("plugin.spring") version "1.9.23"
 }
 
 group = "org.example"
@@ -10,7 +13,11 @@ repositories {
 }
 
 dependencies {
-    testImplementation(kotlin("test"))
+    implementation("org.springframework.boot:spring-boot-starter-web")					//프로젝트 생성하면서 추가한 거
+    implementation("com.fasterxml.jackson.module:jackson-module-kotlin")				//기본적인 종속성
+    implementation("org.jetbrains.kotlin:kotlin-reflect")								//기본적인 종속성
+    annotationProcessor("org.springframework.boot:spring-boot-configuration-processor") //프로젝트 생성하면서 추가한 거
+    testImplementation("org.springframework.boot:spring-boot-starter-test")
 }
 
 tasks.test {
