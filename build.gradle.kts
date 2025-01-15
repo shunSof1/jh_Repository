de1c34fd21ff7b42e8ea2551ad6850eeba47e23e
hyun <shun9067@gmail.com>
2025-01-16 02:55:46 +0900

    가보자고!

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,28 +1,31 @@
 plugins {
-    id("org.springframework.boot") version "2.2.0.RELEASE"
-    id("io.spring.dependency-management") version "1.0.8.RELEASE"
-    kotlin("jvm") version "1.9.23"
-    kotlin("plugin.spring") version "1.9.23"
+    id("java")
+    id("org.springframework.boot") version "3.3.0"
+    id("io.spring.dependency-management") version "1.1.5"
+    kotlin("jvm") version "2.0.0"
+    kotlin("plugin.spring") version "2.0.0"
+    kotlin("plugin.jpa") version "2.0.0"
 }
 
-group = "org.example"
-version = "1.0-SNAPSHOT"
+group = "chart"
+version = "0.0.1-SNAPSHOT"
+
+java {
+    sourceCompatibility = JavaVersion.VERSION_21
+}
 
 repositories {
     mavenCentral()
 }
 
 dependencies {
-    implementation("org.springframework.boot:spring-boot-starter-web")					//프로젝트 생성하면서 추가한 거
-    implementation("com.fasterxml.jackson.module:jackson-module-kotlin")				//기본적인 종속성
-    implementation("org.jetbrains.kotlin:kotlin-reflect")								//기본적인 종속성
-    annotationProcessor("org.springframework.boot:spring-boot-configuration-processor") //프로젝트 생성하면서 추가한 거
+    implementation("org.springframework.boot:spring-boot-starter-thymeleaf")
+    implementation("org.springframework.boot:spring-boot-starter-web")
+    implementation("org.projectlombok:lombok:1.18.28")
+    implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
+    implementation("org.jetbrains.kotlin:kotlin-reflect")
+    implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
     testImplementation("org.springframework.boot:spring-boot-starter-test")
-}
-
-tasks.test {
-    useJUnitPlatform()
-}
-kotlin {
-    jvmToolchain(21)
+    implementation("com.googlecode.json-simple:json-simple:1.1.1")
+    implementation(kotlin("stdlib"))
 }
